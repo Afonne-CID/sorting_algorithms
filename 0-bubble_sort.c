4cb0997e4c222e95371f3d2e8b8c870dc363db42
Afonne-CID <delightinbusiness@gmail.com>
2021-10-22 08:05:39 -0700

    formatting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,22 +5,22 @@
  * bubble_sort - Sorts an array of integers in ascending order
  *       using Bublle sort.
  *@array: The array to sort
- @size: The size of the array
+ *@size: The size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
 	int tmp;
 	size_t i = 0, j = 0;
-	
-	for (i = 0; i < size; i++)	
+
+	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
-			if (array[j] > array[j+1])
+			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
-				array[j] = array[j+1];
-				array[j+1] = tmp;
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
 				print_array(array, size);
 			}
 		}
